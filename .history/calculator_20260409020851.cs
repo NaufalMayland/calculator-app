d3af0891e580a9d00c801d37df7bf0a0234778a4
NaufalMayland <maylandzaa13@gmail.com>
2026-04-09 02:16:55 +0700

    add input

--- .history/calculator_20260409020851.cs
+++ .history/calculator_20260409020851.cs
@@ -2,13 +2,8 @@ class Calculator
 {
     static void Main(string[] args)
     {
-        int a, b;
-
-        Console.WriteLine("Angka pertama: ");
-        a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Angka kedua: ");
-        b = Convert.ToInt32(Console.ReadLine());
-        
+        int a = 10;
+        int b = 6;
 
         Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
         Console.WriteLine("Hasil Penambahan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
